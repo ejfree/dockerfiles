ee597c455b9cc04055e6e7f4e8e21f24eb5f4f58
Erik Freeland <erik.freeland@gmail.com>
2019-11-07 18:17:47 -0800

    Update awsGenerateTLS.sh

--- tlsgenerator/script/awsGenerateTLS.sh
+++ tlsgenerator/script/awsGenerateTLS.sh
@@ -12,26 +12,26 @@ while true; do
 #expected output here is a auth error
 #this will fail on Azure of course
 aws iam get-user  --output json
-sleep 5
+sleep 15
 #AWS API call on port 443 using TLS to show user info
 #aws ec2 describe-vpcs --output json --region us-east-1
-#sleep 5
+#sleep 15
 #Grab EICAR first as binary then as text
 curl --output /dev/null https://secure.eicar.org/eicar.com
-sleep 5
+sleep 15
 #TLS version of TestmyIDS.com
 curl --output /dev/null https://nubevalabs.s3.amazonaws.com/testmyids.txt
-sleep 5
+sleep 15
 #Download Google Homepage via TLS
 curl --output /dev/null https://www.google.com
-sleep 5
+sleep 15
 #Download ESPN Homepage via TLS
 curl --output /dev/null https://www.bbc.com
-sleep 5
+sleep 15
 #Clone Git Repository
 git clone https://github.com/nubevalabs/demorepo.git /tmp/demorepo
 sudo rm -r /tmp/demorepo
-sleep 5
+sleep 15
 #save this for later
 #http://test.com/items.php?id=2 and 1=1
 #This is a SQL injection attempt. The 1=1 is an indicatior. 
